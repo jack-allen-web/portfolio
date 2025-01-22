f5b7d5c97d710416f2af0ee2701b1682cd5f51e9
Jack Allen <jack.allen.web@gmail.com>
2025-01-22 16:47:38 +0000

    Change style to improve UI/UX

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,29 +5,24 @@ import '../styles/Projects.css';
 
 const Projects = () => (
     <section className="projects">
-        <div className="project-box">
-            <h3>Project 1</h3>
-            <p className="tagline">This is a tagline for Project 1.</p>
-        </div>
-        <div className="project-box">
-            <h3>Project 2</h3>
-            <p className="tagline">This is a tagline for Project 2.</p>
-        </div>
-        <div className="project-box">
-            <h3>Project 3</h3>
-            <p className="tagline">This is a tagline for Project 3.</p>
-        </div>
-        <div className="project-box">
-            <h3>Project 4</h3>
-            <p className="tagline">This is a tagline for Project 4.</p>
-        </div>
-        <div className="project-box">
-            <h3>Project 5</h3>
-            <p className="tagline">This is a tagline for Project 5.</p>
-        </div>
-        <div className="project-box">
-            <h3>Project 6</h3>
-            <p className="tagline">This is a tagline for Project 6.</p>
+        <h2 className="projects-title">My Work</h2>
+        <div className="projects-wrapper">
+            <div className="project-box">
+                <h3>Project 1</h3>
+                <p className="tagline">This is a tagline for Project 1.</p>
+            </div>
+            <div className="project-box">
+                <h3>Project 2</h3>
+                <p className="tagline">This is a tagline for Project 2.</p>
+            </div>
+            <div className="project-box">
+                <h3>Project 3</h3>
+                <p className="tagline">This is a tagline for Project 3.</p>
+            </div>
+            <div className="project-box">
+                <h3>Project 4</h3>
+                <p className="tagline">This is a tagline for Project 4.</p>
+            </div>
         </div>
     </section>
 );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,6 +6,7 @@ import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import Footer from "../components/Footer";
 import Contact from "../components/Contact";
+import "../styles/global.css"
 
 const HomePage = () => (
     <>
