31c796bbcacf9d5b54ce692a3b93b0d6dc78b03a
Jack Allen <jack.allen.web@gmail.com>
2025-02-04 17:46:53 +0000

    Remove unneeded imports

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar'
 import Header from "../components/Header";
-import AboutMe from "../components/AboutMe";
-import Skills from "../components/Skills";
-import Footer from "../components/Footer";
-import Contact from "../components/Contact";
 import "../styles/Global.css"
 import Work from "../components/WorkSection";
 
@@ -13,9 +9,6 @@ const HomePage = () => (
         <NavBar />
         <Header />
         <Work />
-        {/*<AboutMe />*/}
-        {/*<Contact />*/}
-        {/*<Footer />*/}
     </>
 );
 
