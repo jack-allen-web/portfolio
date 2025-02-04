96bf6066b7df9ed9f56af3fc205523e5e172c741
Jack Allen <jack.allen.web@gmail.com>
2025-02-04 22:14:43 +0000

    Complete v1 of the AboutMe section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,38 +10,23 @@ const AboutMe = () => {
     };
 
     return (
-        <div className="about-me-container">
-            {/* Header Section */}
-            <div className="about-me-header">
-                <div className="about-me-header-icon">
-                    <img src={profilePic} alt="avatar" />
+        <div className="about-me">
+            <div className="about-me-container">
+                <div className="about-me-header">
+                    <div className="about-me-header-icon">
+                        <img src={profilePic} alt="avatar" />
+                    </div>
+                    <div className="about-me-header-title">
+                        <h3>A bit more about me...</h3>
+                    </div>
                 </div>
-                <div className="about-me-header-title">
-                    <h3>A bit more about me...</h3>
-                </div>
-            </div>
 
-            {/* Description Section */}
-            <div className="about-me-description-container">
-                <p className="about-me-description">
-                    Hey there! 👋 I'm a software engineer who loves to bring ideas to life through code. Whether it's building sleek e-commerce platforms, crafting intuitive mobile apps, or designing robust web applications, I love to create!
-                </p>
-                {/*<button className="read-more-btn" onClick={toggleExpanded}>*/}
-                {/*    {expanded ? 'Read Less' : 'Read More'}*/}
-                {/*</button>*/}
-                {/*{expanded && (*/}
-                {/*    <div className="about-me-expanded-description">*/}
-                {/*        <p>*/}
-                {/*            My toolkit? Oh, it’s packed with <strong>Python</strong>, <strong>Java</strong>, and a sprinkle of creative magic ✨. With a strong foundation in software development and a passion for problem-solving, I’m always up for a new challenge.*/}
-                {/*        </p>*/}
-                {/*        <p>*/}
-                {/*            When I’m not writing code, you’ll probably find me exploring the latest tech trends, enjoying a good cup of coffee, or experimenting with fun new projects.*/}
-                {/*        </p>*/}
-                {/*        <p>*/}
-                {/*            Let’s connect and see what we can build together! 🚀*/}
-                {/*        </p>*/}
-                {/*    </div>*/}
-                {/*)}*/}
+                {/* Description Section */}
+                <div className="about-me-description-container">
+                    <p className="about-me-description">
+                        Hey there! 👋 I'm a software engineer who loves to bring ideas to life through code. Whether it's building sleek e-commerce platforms, crafting intuitive mobile apps, or designing robust web applications, I love to create!
+                    </p>
+                </div>
             </div>
         </div>
     );
